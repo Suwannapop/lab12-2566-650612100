01d3f0bb1491668edbe8edb0c6fdf4410d77db1c
Suppakron <suwannapop01@gmail.com>
2023-08-03 15:12:10 +0700

    let goooo

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,13 +1,21 @@
 import { ThemeContext } from "@/contexts/ThemeContext";
+import { LangContext } from "@/contexts/LangContext";
 import { useContext } from "react";
 
 export const Detail = () => {
   const { theme } = useContext(ThemeContext);
+  const { lang } = useContext(LangContext);
+  let theam;
+  if (theme.name == "light") {
+    theam = lang.detail.lightActivated;
+  } else {
+    theam = lang.detail.darkActivated;
+  }
   return (
     <p className="text-center" style={{ color: theme.fgColor }}>
-      Display activated theme message here (EN or TH)
       {/* tip: Get information from "theme" variable to determine 
       which theme is currently selected */}
+      {theam}
     </p>
   );
 };
